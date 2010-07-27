35bcbb0db93bba6c2fe304d4afe776bc6e417268
Nicolas Gramlich <nicolasgramlich@gmail.com>
2010-07-27 14:37:11 +0200

    Compatibility to the latest andengine.jar and andenginelivewallpaper.jar .

--- src/org/anddev/wallpaper/live/cigarette/LiveWallpaperService.java
+++ src/org/anddev/wallpaper/live/cigarette/LiveWallpaperService.java
@@ -4,11 +4,11 @@ import org.anddev.andengine.engine.camera.Camera;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
-import org.anddev.andengine.entity.Scene;
 import org.anddev.andengine.entity.particle.ParticleSystem;
 import org.anddev.andengine.entity.particle.modifier.AlphaModifier;
 import org.anddev.andengine.entity.particle.modifier.ExpireModifier;
-import org.anddev.andengine.entity.particle.modifier.VelocityModifier;
+import org.anddev.andengine.entity.particle.modifier.VelocityInitializer;
+import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.extension.ui.livewallpaper.BaseLiveWallpaperService;
 import org.anddev.andengine.opengl.texture.Texture;
@@ -38,7 +38,7 @@ public class LiveWallpaperService extends BaseLiveWallpaperService implements IA
 	private TextureRegion mCigaretteTextureRegion;
 
 	private ScreenOrientation mScreenOrientation;
-	private VelocityModifier mVelocityModifier;
+	private VelocityInitializer mVelocityInitializer;
 
 	// ===========================================================
 	// Constructors
@@ -66,13 +66,12 @@ public class LiveWallpaperService extends BaseLiveWallpaperService implements IA
 		this.mSmokeTextureRegion = TextureRegionFactory.createFromAsset(this.mTexture, this, "gfx/smoke.png", 381, 0); // 64x64
 		
 		this.getEngine().getTextureManager().loadTexture(this.mTexture);
-		this.enableAccelerometer(this);
+		this.enableAccelerometerSensor(this);
 	}
 
 	@Override
 	public Scene onLoadScene() {
 		final Scene scene = new Scene(2);
-		scene.setBackgroundColor(0, 0, 0);
 
 		final int cigaretteX = 0;
 		final int cigaretteY = CAMERA_HEIGHT - this.mCigaretteTextureRegion.getHeight();
@@ -83,8 +82,8 @@ public class LiveWallpaperService extends BaseLiveWallpaperService implements IA
 		final int smokeY = cigaretteY - 25;
 		final ParticleSystem smokeParticleSystem = new ParticleSystem(smokeX, smokeY, 0, 0, 6, 8, 150, this.mSmokeTextureRegion);
 		smokeParticleSystem.addParticleModifier(new ExpireModifier(8,10));
-		this.mVelocityModifier = new VelocityModifier(-20, 20, -100, -120);
-		smokeParticleSystem.addParticleModifier(this.mVelocityModifier);
+		this.mVelocityInitializer = new VelocityInitializer(-20, 20, -100, -120);
+		smokeParticleSystem.addParticleInitializer(this.mVelocityInitializer);
 		smokeParticleSystem.addParticleModifier(new AlphaModifier(1, 0, 0, 10));
 		
 //		this.getEngine().registerPreFrameHandler(new FPSCounter());
@@ -106,7 +105,7 @@ public class LiveWallpaperService extends BaseLiveWallpaperService implements IA
 		
 		final float minVelocityY = (pAccelerometerData.getY() - 8) * 10;
 		final float maxVelocityY = (pAccelerometerData.getY() - 10) * 10;
-		this.mVelocityModifier.setVelocity(minVelocityX, maxVelocityX, minVelocityY, maxVelocityY);
+		this.mVelocityInitializer.setVelocity(minVelocityX, maxVelocityX, minVelocityY, maxVelocityY);
 	}
 
 	// ===========================================================
